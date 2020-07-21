b9e4e91b6ec3e0f11011e8f5955efdbd2af23735
Shrey Jain <jainshreyp@gmail.com>
2020-07-21 14:00:56 -0400

    added graphql and express server using ip address

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+var ip = require('ip');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, ip.address(), () => console.log('Now browse to' + ip.address() + ':4000/graphql'));
